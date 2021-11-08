b03fea8a1c3a21da90daad3644d072d9d28fd2f6
@itsAj <ajaymati1000@gmail.com>
2021-11-08 15:44:47 +0530

    last

---
+++ Min-Sum/main.cpp
@@ -0,0 +1,74 @@
+// @itsAj
+#include <bits/stdc++.h>
+//#include<boost/multiprecision/cpp_int.hpp>
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+#define test  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define pb push_back
+#define endl '\n'
+#define rep(i, x, y) for (auto i = x; i <= y; i++)
+#define repi(i, x, y) for (auto i = x; i >= y; i--)
+#define for_each(v, a) for (auto &v : a)
+#define arr_in(a) for_each(v, a) cin >> v
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define u_map unordered_map
+#define u_set unordered_set
+
+// define the size of the set bit
+#define B 40
+//
+
+#define mod 1000000007
+
+typedef unsigned long long ull;
+typedef long long ll;
+using namespace std;
+// using namespace boost::multiprecision;
+
+template <typename T>
+inline T modv(T v, T m) { return ((v % m) + m) % m; }
+
+/*
+    workplace start from here
+*/
+
+int solve()
+{
+    // The most optimal answer is to make a chain
+    int n;
+    cin >> n;
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 2;
+
+    return (3 * 2 + (n - 3) * 4);
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    fastio;
+
+    test
+    {
+        cout << solve();
+        cout << endl;
+    }
+
+    return 0;
+}
